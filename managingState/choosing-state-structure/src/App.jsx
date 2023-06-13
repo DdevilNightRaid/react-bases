132211484dfb06f31885e6af670cf106d370e8f0
DDevil NightRaid <ddevil@gmail.com>
2023-06-13 09:34:09 +0545

    Avoid Deeply Nested State

--- managingState/choosing-state-structure/src/App.jsx
+++ managingState/choosing-state-structure/src/App.jsx
@@ -5,20 +5,24 @@ import WithRedundant from './avoid-redundant-state/WithRedundant'
 import WithOutRedundant from './avoid-redundant-state/WithOutRedundant'
 import WithDuplication from './avoid-duplication-in-state/WithDuplication'
 import WithOutDuplication from './avoid-duplication-in-state/WithOutDuplication'
+import Deeply from './avoid-deeply-nested-state/DeeplyNested/Deeply'
+import NotDeeply from './avoid-deeply-nested-state/NotDeeply/NotDeeply'
 function App() {
 
   return (
     <>
-     <FollowMouse/>
-     <hr />
-     <WithOut/> 
-     <hr />
-     <WithRedundant/> 
-     <WithOutRedundant/> 
-     <hr />
-     <WithDuplication/> 
-     <WithOutDuplication/> 
-     
+      <FollowMouse />
+      <hr />
+      <WithOut />
+      <hr />
+      <WithRedundant />
+      <WithOutRedundant />
+      <hr />
+      <WithDuplication />
+      <WithOutDuplication />
+      <hr />
+      <Deeply />
+      <NotDeeply />
     </>
   )
 }
